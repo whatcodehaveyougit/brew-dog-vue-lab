d6cc7579d18369eb1c3ae9d933a5766093a8a44c
whatcodehaveyougit <siggywatt@yahoo.com>
2020-01-16 12:35:45 +0000

    array of objects

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+<p>jfjfj</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+    return {
+      beers: []
+    }
+  },
+  mounted() {
+    fetch('https://api.punkapi.com/v2/beers')
+    .then(response => response.json())
+    .then(beers => this.beers = beers )
   }
+
+
 }
+
 </script>
 
 <style>
